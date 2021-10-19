0fae00dca324d13d39ce80299d3390ecc87579de
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-10-19 15:33:10 +0100

    updates

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import listEndpoints from "express-list-endpoints"
-import authorsRouter from "./services/index.js"
+import authorsRouter from "./services/authors/index.js"
 
 
 const server = express()
